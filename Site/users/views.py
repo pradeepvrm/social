1abf43f89c90be437a7885a8d21c9bda5752b79a
pradeepkv012 <pradeepkv012@gmail.com>
2025-06-11 11:09:32 +0000

    initial commit

---
+++ Site/users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .forms import LoginForm
+
+# Create your views here.
+def user_login(request):
+    form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
+
