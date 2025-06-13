18df04d375ab3bcdf01fff5e551f32fc3d7ff8ba
pradeepkv012 <pradeepkv012@gmail.com>
2025-06-13 11:30:24 +0000

    implemented login/logout

--- Site/users/views.py
+++ Site/users/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def user_login(request):
-    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password']) 
+            if user is not None:
+                login(request, user)
+                return HttpResponse('user authenticated!!')
+            else:
+                return HttpResponse('invalid login')
+    else:
+        form = LoginForm()
     return render(request, 'users/login.html', {'form': form})
 
+@login_required
+def index(request):
+    return render(request, 'users/index.html')
