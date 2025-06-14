9743d401e5c2584fba014399ea03440d80fc3af0
pradeepkv012 <pradeepkv012@gmail.com>
2025-06-14 08:50:07 +0000

    added register functionality

--- Site/users/urls.py
+++ Site/users/urls.py
@@ -1,7 +1,7 @@
 from re import template
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
@@ -10,5 +10,10 @@ urlpatterns = [
     path('password_change/', PasswordChangeView.as_view(template_name='users/password_change_form.html'), name='password_change'),
     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
     path('password_reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html'), name='password_reset'),
-    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done')
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('reset<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+
+    path('register/', views.register, name='register'),
+
+    path('edit/', views.edit, name='edit'),
 ]
