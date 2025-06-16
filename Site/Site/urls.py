87beab8bff5f5e23092fca2d8fc084b9fd3a1044
pradeepkv012 <pradeepkv012@gmail.com>
2025-06-16 17:51:46 +0000

    added posts url

--- Site/Site/urls.py
+++ Site/Site/urls.py
@@ -7,7 +7,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('post/', include('posts.urls')),
 ] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
