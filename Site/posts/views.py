a3847b550b46d3f79c2c1e42649eb029990b7205
pradeepkv012 <pradeepkv012@gmail.com>
2025-06-18 11:48:40 +0000

    updated feed view

--- Site/posts/views.py
+++ Site/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import PostCreateForm
 from django.contrib.auth.decorators import login_required
+from .models import Post
 
 # Create your views here.
 
