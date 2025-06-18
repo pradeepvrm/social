9eafce838d87eee2095e9c7cbe160450d7d41769
pradeepkv012 <pradeepkv012@gmail.com>
2025-06-18 17:32:30 +0000

    added comment functionality

--- Site/posts/urls.py
+++ Site/posts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create', views.post_create, name='create'),
+    path('feed', views.feed, name='feed'),
+    path('like', views.like_post, name='like'),
 ]
